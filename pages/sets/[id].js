6ea01b108087961012ea487a787cc36790d08f2f
Zander <steamwings@outlook.com>
2022-10-27 23:40:37 -0400

    Dynamic note and title; use humanized song names in yaml

--- pages/sets/[id].js
+++ pages/sets/[id].js
@@ -5,19 +5,21 @@ import Layout from '../../components/layout'
 import Song from '../../components/song'
 import utilStyles from '../../styles/utils.module.css'
 
-export default function set({ title, readings, songs }) {
+export default function set({ title, note, readings, songs }) {
   return (
     <Layout hideBackToHome>
       <h1>{title}</h1>
-      <p className={utilStyles.note}>Thanks for joining us!&nbsp;</p>
-      <div>
-       <h3>Recommended Readings</h3>
-        <ul>
-          {
-            readings.map(r => (<li key={r}>{r}</li>))
-          }
-        </ul> 
-      </div>
+      <p className={utilStyles.note}>{note} &nbsp;</p>
+      { readings && readings.length > 0 && 
+        <div>
+          <h3>Recommended Readings</h3>
+          <ul>
+            {
+              readings.map(r => (<li key={r}>{r}</li>))
+            }
+          </ul> 
+        </div> 
+      }
       {
         songs.map((song) => (<div key={song.name}>
           <h3>Lyrics</h3>
@@ -34,24 +36,29 @@ export async function getStaticProps(context) {
   const { id } = context['params']
   const setDir = path.join(process.cwd(), 'private', 'sets');
   const content = yaml.load(await fs.readFile(setDir + '/' + id + '.yml'));
-  const { title, readings, songs } = content;
+  const { title, note, readings, songs } = content;
 
   const songDir = path.join(process.cwd(), 'public', 'lyrics');
   const songObjs = await Promise.all(songs.map(async (song) => {
-    const fileContents = await fs.readFile(songDir + '/' + song + '.txt', 'utf8');
+    const fileName = songDir + '/' + dehumanize(song) + '.txt'
+    const fileContents = await fs.readFile(fileName, 'utf8')
+      .catch(error => 'Failed to retrieve song lyrics. :(');
     return {
-      name: song.split('_').map(s => s[0].toUpperCase() + s.substring(1)).join(' '), // TODO How to put this in a function?
+      name: song,
       lyrics: fileContents
     }
   }))
 
   return {
-    props: { title, readings, songs: songObjs }
+    props: { 
+      title: title || toLongDate(id), 
+      note: note || "Thanks for joining us! Gracias por venir!", 
+      readings: readings || null, 
+      songs: songObjs }
   }
 }
 
 export async function getStaticPaths() {
-  console.log('getStaticPaths')
   const dir = path.join(process.cwd(), 'private', 'sets');
   const files = await fs.readdir(dir);
   const result = files.map((f) => ({params: {id: f.split('.')[0]}}))
@@ -60,4 +67,16 @@ export async function getStaticPaths() {
     paths: result,
     fallback: false
   }
+}
+
+function dehumanize(str) {
+  return str.toLowerCase().replaceAll(/\s/g, '_').replaceAll(/[^\w]/g, '')
+}
+
+function toLongDate(id) {
+  const date = Date.UTC(id.slice(0,4), id.slice(4,6) - 1, id.slice(6,8), 5, 0, 0)
+  return new Date(date)
+    .toLocaleDateString("en-US", 
+    { year: 'numeric', month: 'long', day: 'numeric' }
+  )
 }
