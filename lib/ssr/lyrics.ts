b255cf66fac54e08cd05ca122411e38f978fe32f
Zander <steamwings@outlook.com>
2023-10-18 16:44:09 -0400

    Cleanup lib/ssr/lyrics and throw error at SSR when lyrics aren't found

--- lib/ssr/lyrics.ts
+++ lib/ssr/lyrics.ts
@@ -1,22 +1,39 @@
 import path from 'path'
-import { promises as fs } from 'fs'
-import { cache } from 'react'
+import {promises as fs} from 'fs'
+import {cache} from 'react'
 export const MY_LYRICS_DIR = path.join(process.cwd(), 'private', 'my_lyrics');
 export const NOT_MY_LYRICS_DIR = path.join(process.cwd(), 'private', 'not_my_lyrics');
+
+async function listLyricFiles(dir : string, attrs : {
+  mine : boolean
+}) {
+  const files = await fs.readdir(dir);
+  return files.map(file => ({
+    title: file.split('.')[0],
+    ...attrs
+  }));
+}
+
 async function _listMyLyrics() {
-  const files = await fs.readdir(MY_LYRICS_DIR);
-  return files.map(file => ({ title: file.split('.')[0], mine: true }) );
+  return listLyricFiles(MY_LYRICS_DIR, {mine: true});
 }
+
 async function _listLyrics() {
-  const files = await fs.readdir(NOT_MY_LYRICS_DIR);
+  const notMyLyrics = await listLyricFiles(NOT_MY_LYRICS_DIR, {mine: false});
   const myLyrics = await listMyLyrics();
-  return files.map(file => ({ title: file.split('.')[0], mine: false }) ).concat(myLyrics);
+  return notMyLyrics.concat(myLyrics);
 }
-export async function getLyric(title: string) {
-  const mine = (await listLyrics()).find(lyric => lyric.title === title)?.mine || false;
+
+export async function getLyric(title : string) {
+  const lyric = (await listLyrics()).find(lyric => lyric.title === title);
+  if (lyric == undefined) 
+    throw new Error(`No lyrics found for ${title}`);
+  
+  const mine = lyric.mine;
   const dir = mine ? MY_LYRICS_DIR : NOT_MY_LYRICS_DIR;
   const content = await fs.readFile(path.join(dir, `${title}.txt`), 'utf-8');
-  return { content, mine };
+  return {content, mine};
 }
+
 export const listMyLyrics = cache(_listMyLyrics);
 export const listLyrics = cache(_listLyrics);
