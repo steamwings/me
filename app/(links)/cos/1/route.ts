60ba46d6b23256611ce84d8a2dfc186e9953b487
Zander <steamwings@outlook.com>
2024-02-03 13:38:21 -0500

    Link to CoS session 1

---
+++ app/(links)/cos/1/route.ts
@@ -0,0 +1,5 @@
+import {NextResponse} from "next/server";
+
+export async function GET(_req : Request) {
+  return NextResponse.redirect("https://1drv.ms/u/s!AiBsX5kO4VFLu7VDz3MED6NMRpsD3A?e=O0zOxV");
+}
