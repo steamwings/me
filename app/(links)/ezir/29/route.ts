c0f41f138443e18bb52c5cd3e313244c07f74c44
Zander <steamwings@outlook.com>
2024-02-15 21:42:02 -0500

    ezir 29

---
+++ app/(links)/ezir/29/route.ts
@@ -0,0 +1,5 @@
+import {NextResponse} from "next/server";
+
+export async function GET(_req : Request) {
+  return NextResponse.redirect("https://1drv.ms/u/s!AiBsX5kO4VFLu7c3dSvUl4v2FMvXzQ?e=fvajqS");
+}
