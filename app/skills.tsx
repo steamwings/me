b23c50abc2ad9d2eb45947255db849949f6b0c73
Zander <steamwings@outlook.com>
2024-02-23 15:57:54 -0500

    better black

--- app/skills.tsx
+++ app/skills.tsx
@@ -6,7 +6,7 @@ export default function CV() {
     <Layout>
       <h1>Skills</h1>
       <p className={utilStyles.note}>This list is not exhaustive.&nbsp;
-      <a className={utilStyles.linkBlue}
+      <a
         href="mailto:contact@zweather.me"
         aria-label="email link">Ask me</a>
       &nbsp;if I&apos;ve used your technology.</p>
@@ -64,7 +64,7 @@ export default function CV() {
       </div>
       <div className={utilStyles.note}>
         Check out my (now defunct)&nbsp;
-        <a className={utilStyles.linkBlue}
+        <a
           href="https://github.com/steamwings?tab=repositories&q=gc&type=&language=&sort="
           aria-label="Green Curtain project">Green Curtain</a>
         &nbsp;project, built with NativeScript + Angular, Azure Functions, and Cosmos DB.
