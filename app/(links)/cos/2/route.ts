7cda2ee3f8b9be5e3f49da425182c8a8f4341197
Zander <steamwings@outlook.com>
2024-02-24 23:15:32 -0500

    cos 2

---
+++ app/(links)/cos/2/route.ts
@@ -0,0 +1,5 @@
+import {NextResponse} from "next/server";
+
+export async function GET(_req : Request) {
+  return NextResponse.redirect("https://1drv.ms/u/s!AiBsX5kO4VFLu7hqgLjybbVXQHIJ7Q?e=8pEvzv");
+}
