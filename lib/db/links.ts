56ac7190e12493d78ec5679bb8b03af88746c6e4
Zander <steamwings@outlook.com>
2024-02-27 21:48:46 -0500

    Step 3: cleanup, more verbs, handle error

--- lib/db/links.ts
+++ lib/db/links.ts
@@ -11,30 +11,37 @@ export function getLink(filter: object) {
 }
 
 export function getLinks(pathRegex: string | null = null) {
-  try {
-    const filter = pathRegex == null ? {} : { "path": { "$regex": pathRegex} };
-    return getMongoClient().db('me').collection('links')
-      .find(filter).toArray();
-  } catch (e) {
-    console.error(e);
-    return [];
-  }
+  const filter = pathRegex == null ? {}
+    : { "path": { "$regex": pathRegex} };
+  return getMongoClient().db('me').collection('links')
+    .find(filter).toArray();
 }
 
 export function addLink(path: string, destination: string) {
-  return getMongoClient().db('me').collection('links').insertOne(
-    {
-      path, destination,
-      stats: {refs: 0},
-      createdAt: new Date(),
-      updatedAt: new Date()
-    }
-  );
+  return getMongoClient().db('me').collection('links')
+    .insertOne(
+      {
+        path, destination,
+        stats: {refs: 0},
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+    );
 }
 
-export function updateLink(path: string, update: object) {
+export function updateLink(path: string, destination: string) {
   return getMongoClient().db('me').collection('links')
-    .updateOne({ path }, { $set: update });
+    .updateOne({ path },
+      { $set: { destination },
+        $currentDate: { updatedAt: true }
+      });
+}
+
+export async function deleteLink(path: string) {
+  const db = getMongoClient().db('me');
+  const doc = await db.collection('links').findOneAndDelete({ path });
+  return await db.collection('links-archive')
+    .insertOne({ ...doc, deletedAt: new Date() });
 }
 
 function getMongoClient() {
