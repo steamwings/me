9cb5a2d2f7e3056bc3f8a45f98c8cfb3cd11bae4
Zander <steamwings@outlook.com>
2024-02-27 22:04:35 -0500

    remove remaining hardcoded shortlinks

--- app/(links)/ezir/28/route.ts
+++
@@ -1,5 +0,0 @@
-import {NextResponse} from "next/server";
-
-export async function GET(_req : Request) {
-  return NextResponse.redirect("https://1drv.ms/u/s!AiBsX5kO4VFLu7YvevJ0Q51IgqUDxA?e=aHkz6c");
-}
--- app/(links)/ezir/29/route.ts
+++
@@ -1,5 +0,0 @@
-import {NextResponse} from "next/server";
-
-export async function GET(_req : Request) {
-  return NextResponse.redirect("https://1drv.ms/u/s!AiBsX5kO4VFLu7c3dSvUl4v2FMvXzQ?e=fvajqS");
-}
