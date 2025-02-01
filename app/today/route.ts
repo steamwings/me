41d517e6db491ed239813808ffa52dabe1d8ba7f
Zander <steamwings@outlook.com>
2025-02-01 00:44:17 -0500

    Revert "quick link for Immanuel"
    This reverts commit b1282967e56ef70a6722636e2ac2ed9baeb56c36.

--- app/today/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextRequest, NextResponse } from "next/server";
-
-export async function GET(req : NextRequest, _props: { params: Promise<{ link: string }> }) {
-  const url = req.nextUrl.clone()
-  url.pathname = '/sets/20250201'
-  return NextResponse.redirect(url);
-}
-
